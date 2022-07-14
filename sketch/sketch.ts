494f2caaaf8ddc98acbda3353acb0ed887c7b830
nbogie <neillbogie@googlemail.com>
2022-07-14 15:58:12 +0100

    demonstrated the use of filtering the calculated list, in sketch.ts

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -5,18 +5,54 @@ function setup() {
 
 function draw() {
   background("white");
-  const circlesDataArray = calculatePackedCircles(width, height);
-  for (const c of circlesDataArray) {
-    drawCircle(c);
-  }
-  //bonus note: you could also filter the circlesDataArray before drawing,
-  //to obtain only circles which meet criteria around certain radius or position
+  const allCirclesData = calculatePackedCircles(width, height);
+  // drawDesign1(allCirclesData);
+  drawDesign2(allCirclesData);
 }
 
-function drawCircle(c: CircleData) {
+//This introduces the use of filter
+function drawDesign1(allCirclesData: CircleData[]) {
+  const circlesDataToDraw = allCirclesData.filter(isWithinRange);
+
+  function isWithinRange(c: CircleData) {
+    const canvasCentrePos = { x: width / 2, y: height / 2 };
+    return distance(c.pos, canvasCentrePos) < 200;
+  }
+
+  for (const c of circlesDataToDraw) {
+    drawCircle(c, "filled");
+  }
+}
+
+//this uses filters a bit more
+function drawDesign2(allCircles: CircleData[]) {
+  const centre = { x: width / 2, y: height / 2 };
+
+  function isWithinRange(c: CircleData) {
+    return distance(c.pos, centre) < 200;
+  }
+
+  const leftCircles = allCircles.filter(
+    (c) => c.pos.x < centre.x && isWithinRange(c)
+  );
+  const rightCircles = allCircles.filter(
+    (c) => c.pos.x > centre.x && !isWithinRange(c)
+  );
+
+  for (const c of leftCircles) {
+    drawCircle(c, "filled");
+  }
+  for (const c of rightCircles) {
+    drawCircle(c, "empty");
+  }
+}
+
+/** a type (a "union type") of OUR creation,
+ * used to communicate whether we should fill or outline a a shape */
+type FillStyle = "filled" | "empty";
+function drawCircle(c: CircleData, fillStyle: FillStyle) {
   const shadeOfGray = random(50, 100);
-  //if on left hand side of canvas...
-  if (c.pos.x < width / 2) {
+  if (fillStyle === "filled") {
     fill(shadeOfGray);
     noStroke();
   } else {
